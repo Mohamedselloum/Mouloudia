0108bbc1bb4b49e1ee973ecc55f31e160d45db2c
Mohamedselloum <mohsell2003@gmail.com>
2025-05-05 10:59:31 +0100

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-500 to-purple-600 text-white text-4xl font-bold">
+      Welcome to Milan Store
+    </div>
+  )
+}
+
+export default App
